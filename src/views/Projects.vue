2dda682b241d7bbd7ae6de83ea9cfb03485d38bf
Daren McCulley <mcculleydj@gmail.com>
2020-11-11 14:53:14 -0500

    projects page

---
+++ src/views/Projects.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <h1>Projects</h1>
+    <v-list>
+      <v-list-item
+        v-for="project in projects"
+        :key="project.name"
+        :to="project.link"
+      >
+        <v-list-item-content>
+          <v-list-item-title>{{ project.name }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Projects',
+
+  data: () => ({
+    projects: [{ name: 'Mastering D3', link: '/' }],
+  }),
+}
+</script>
