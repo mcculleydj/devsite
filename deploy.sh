0573a3e9f72b0ac27e6bbe456073fcf0c0fc24da
Daren McCulley <mcculleydj@gmail.com>
2020-11-11 16:21:48 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 echo "Building and deploying the Vue application..."
 yarn run build
+ssh -i ~/.ssh/mcculleydj.dev.pem ubuntu@54.203.119.188 rm -rf /home/ubuntu/devsite
 scp -r -i ~/.ssh/mcculleydj.dev.pem dist ubuntu@54.203.119.188:/home/ubuntu/devsite
 ssh -i ~/.ssh/mcculleydj.dev.pem ubuntu@54.203.119.188 sudo systemctl restart nginx
