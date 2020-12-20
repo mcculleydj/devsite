67f5aadf94de37e861fb0296083e82c97d5ae415
Daren McCulley <mcculleydj@gmail.com>
2020-12-20 10:52:23 -0500

    rsync

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "Building and deploying the Vue application..."
+
 yarn run build
-ssh -i ~/.ssh/mcculleydj.dev.pem ubuntu@54.203.119.188 rm -rf /home/ubuntu/devsite
-scp -r -i ~/.ssh/mcculleydj.dev.pem dist ubuntu@54.203.119.188:/home/ubuntu/devsite
+
+rsync --delete -azv -e "ssh -i ~/.ssh/mcculleydj.dev.pem" dist/ ubuntu@54.203.119.188:/home/ubuntu/devsite
+
 ssh -i ~/.ssh/mcculleydj.dev.pem ubuntu@54.203.119.188 sudo systemctl restart nginx
+
+rm -rf dist
