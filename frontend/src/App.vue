16d4152376109caa9b3b637b7268e9de401902c1
Daren McCulley <mcculleydj@gmail.com>
2021-01-24 09:26:30 -0500

    mobile warning

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <v-app>
     <v-main id="main">
-      <router-view />
+      <v-container class="hidden-sm-and-up">
+        <v-row>
+          <v-col>
+            Sorry, but this site is not mobile friendly. My goal was to get
+            something online quickly ahead of job applications that hiring teams
+            could review at their desk. Eventually, I will support smaller
+            viewports, but it was not a first priority. That said, this site is
+            still responsive. If you are seeing this on laptop or desktop,
+            simply increase the size of your window.
+          </v-col>
+        </v-row>
+      </v-container>
+      <router-view class="hidden-xs-only" />
     </v-main>
   </v-app>
 </template>
--- frontend/src/views/Sandbox.vue
+++ frontend/src/views/Sandbox.vue
@@ -1,105 +1,66 @@
 <template>
-  <div class="canvas">
-    <canvas id="canvas" class="canvas__canvas"></canvas>
-  </div>
+  <v-container fluid>
+    <!-- <v-row justify="center" class="mt-12">
+      <div class="project" style="transform: rotateY(-60deg) scale(.6)">
+        <v-img src="rosetta.png" max-width="250" class="project__image" />
+      </div>
+      <div class="project" style="transform: rotateY(-30deg) scale(.8)">
+        <v-img src="rosetta.png" max-width="250" class="project__image" />
+      </div>
+      <div class="project">
+        <v-img src="rosetta.png" max-width="250" class="project__image" />
+      </div>
+      <div class="project" style="transform: rotateY(30deg) scale(.8)">
+        <v-img src="rosetta.png" max-width="250" class="project__image" />
+      </div>
+      <div class="project" style="transform: rotateY(60deg) scale(.6)">
+        <v-img src="rosetta.png" max-width="250" class="project__image" />
+      </div>
+    </v-row> -->
+    <div style="margin: 500px 500px; display: inline-block">
+      <div class="project">
+        <v-img src="rosetta.png" max-width="250" class="project__image" />
+      </div>
+    </div>
+  </v-container>
 </template>
 
 <script>
-function drawPolygon(ctx, pts, radius) {
-  if (radius > 0) {
-    pts = getRoundedPoints(pts, radius)
-  }
-
-  let pt
-
-  ctx.beginPath()
-
-  for (let i = 0; i < pts.length; i++) {
-    pt = pts[i]
-
-    if (i == 0) {
-      ctx.moveTo(pt[0], pt[1])
-    } else {
-      ctx.lineTo(pt[0], pt[1])
-    }
-
-    if (radius > 0) {
-      ctx.quadraticCurveTo(pt[2], pt[3], pt[4], pt[5])
-    }
-  }
-
-  ctx.closePath()
-}
-
-function getRoundedPoints(pts, radius) {
-  const len = pts.length
-  const res = new Array(len)
-
-  for (let i2 = 0; i2 < len; i2++) {
-    let i1 = i2 - 1
-    let i3 = i2 + 1
-
-    if (i1 < 0) {
-      i1 = len - 1
-    }
-
-    if (i3 == len) {
-      i3 = 0
-    }
-
-    const p1 = pts[i1]
-    const p2 = pts[i2]
-    const p3 = pts[i3]
-    const prevPt = getRoundedPoint(p1[0], p1[1], p2[0], p2[1], radius, false)
-    const nextPt = getRoundedPoint(p2[0], p2[1], p3[0], p3[1], radius, true)
-    res[i2] = [prevPt[0], prevPt[1], p2[0], p2[1], nextPt[0], nextPt[1]]
-  }
-
-  return res
-}
-
-function getRoundedPoint(x1, y1, x2, y2, radius, first) {
-  const total = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
-  const idx = first ? radius / total : (total - radius) / total
-  return [x1 + idx * (x2 - x1), y1 + idx * (y2 - y1)]
-}
-
 export default {
   mounted() {
-    const canvas = document.getElementById('canvas')
-    const ctx = canvas.getContext('2d')
-    ctx.strokeStyle = '#ccc'
-    ctx.lineWidth = 5
-    const sideLength = 50
-    const k = Math.sqrt(3) / 2
-
-    const vertices = [
-      [0, -sideLength],
-      [k * sideLength, -0.5 * sideLength],
-      [k * sideLength, 0.5 * sideLength],
-      [0, sideLength],
-      [-k * sideLength, 0.5 * sideLength],
-      [-k * sideLength, -0.5 * sideLength],
-    ]
-
-    const transformedVertices = vertices.map(([x, y]) => [x + 100, y + 100])
-
-    drawPolygon(ctx, transformedVertices, 10)
-    ctx.stroke()
+    window.addEventListener('scroll', e => {
+      console.log(e)
+    })
   },
 }
 </script>
 
 <style scoped>
-.canvas {
-  width: 100%;
-  height: 100%;
+.project {
+  display: inline-block;
+  height: 300px;
+  width: 300px;
+  border-radius: 50%;
   position: relative;
+  transition: all 0.4s;
+  background-color: white;
+  box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.4);
 }
 
-.canvas__canvas {
+.project:hover {
+  transform: scale(1.4);
+  box-shadow: 0 2rem 4rem rgba(0, 0, 0, 0.8);
+  filter: brightness(60%);
+}
+
+.project__image {
   position: absolute;
-  top: 100px;
-  left: 100px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.project:hover .project__image {
+  filter: blur(5px);
 }
 </style>
